4129eb3444832a2e5a589c03a7054fa29de6dfd2
Mike Johnston <mrbean355@gmail.com>
2023-12-01 17:38:41 +0200

    Add framework code

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,39 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register("generateDayTemplate") {
+    doLast {
+        val day = findProperty("day")?.toString()?.toInt()
+            ?: error("Missing day, use -Pday=x")
+
+        copyTemplate("main", "Day%d.kt", "main_source.txt", day)
+        copyTemplate("test", "Day%dTest.kt", "test_source.txt", day)
+        createEmptyResources(day)
+    }
+}
+
+fun copyTemplate(dir: String, file: String, template: String, day: Int) {
+    val path = "src/$dir/kotlin/com/github/mrbean355/aoc/day$day/".also {
+        file(it).mkdirs()
+    }
+
+    val output = file("$path/${file.format(day)}")
+    if (!output.exists()) {
+        val content = file("templates/$template").readText()
+        output.writeText(content.replace("\$DAY$", day.toString()))
+    }
+}
+
+fun createEmptyResources(day: Int) {
+    val resources = file("src/test/resources/day$day/").apply { mkdirs() }
+    file("$resources/example.txt").takeIf { !it.exists() }?.writeText("")
+    file("$resources/puzzle.txt").takeIf { !it.exists() }?.writeText("")
 }
---
+++ src/main/kotlin/com/github/mrbean355/aoc/base/Puzzle.kt
@@ -0,0 +1,9 @@
+package com.github.mrbean355.aoc.base
+
+interface Puzzle {
+
+    fun part1(): Any
+
+    fun part2(): Any
+
+}
