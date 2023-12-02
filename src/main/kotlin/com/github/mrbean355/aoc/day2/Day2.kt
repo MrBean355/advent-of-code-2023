94c8c45ba4e2e0f8b391d273855ce6795648a35f
Mike Johnston <mrbean355@gmail.com>
2023-12-02 15:40:58 +0200

    Implement day 2 part 1 solution

---
+++ src/main/kotlin/com/github/mrbean355/aoc/day2/Day2.kt
@@ -0,0 +1,60 @@
+package com.github.mrbean355.aoc.day2
+
+import com.github.mrbean355.aoc.base.Puzzle
+
+class Day2(
+    private val input: List<String>,
+) : Puzzle {
+
+    override fun part1(): Int {
+        return input.map(Game::from).filter { game ->
+            game.draws.all { draw ->
+                draw.all {
+                    it.second <= when (it.first) {
+                        Cube.Red -> 12
+                        Cube.Green -> 13
+                        Cube.Blue -> 14
+                    }
+                }
+            }
+        }.sumOf(Game::id)
+    }
+
+    override fun part2(): Any {
+        TODO("Not yet implemented")
+    }
+}
+
+private data class Game(
+    val id: Int,
+    val draws: List<List<Pair<Cube, Int>>>,
+) {
+    companion object {
+
+        fun from(input: String): Game {
+            val colon = input.indexOf(':')
+            val id = input.substring(5, colon).toInt()
+            val draws = input.substring(colon + 2)
+
+            val m = draws.split("; ").map { draw ->
+                draw.split(", ").map {
+                    val amount = it.substringBefore(' ').toInt()
+                    val colour = it.substringAfter(' ')
+                    val cube = when (colour) {
+                        "red" -> Cube.Red
+                        "green" -> Cube.Green
+                        "blue" -> Cube.Blue
+                        else -> error("Unexpected colour: $colour")
+                    }
+                    cube to amount
+                }
+            }
+
+            return Game(id, m)
+        }
+    }
+}
+
+private enum class Cube {
+    Red, Green, Blue
+}
---
+++ src/test/kotlin/com/github/mrbean355/aoc/day2/Day2Test.kt
@@ -0,0 +1,15 @@
+package com.github.mrbean355.aoc.day2
+
+import com.github.mrbean355.aoc.base.PuzzleTest
+
+class Day2Test : PuzzleTest(Day2::class) {
+
+    override val part1TestCases = mapOf(
+        "day2/example.txt" to 8,
+        "day2/puzzle.txt" to 2617,
+    )
+
+    override val part2TestCases: Map<String, Any>
+        get() = emptyMap()
+
+}
