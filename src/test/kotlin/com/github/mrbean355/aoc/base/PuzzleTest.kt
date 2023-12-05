d3e122bb79749d85b71187f1b951020553034c4f
Mike Johnston <mrbean355@gmail.com>
2023-12-05 18:23:38 +0200

    Measure how long the solutions take to execute

--- src/test/kotlin/com/github/mrbean355/aoc/base/PuzzleTest.kt
+++ src/test/kotlin/com/github/mrbean355/aoc/base/PuzzleTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.fail
+import kotlin.time.measureTimedValue
 
 /**
  * Base test class to make unit testing puzzles easier.
@@ -20,35 +21,36 @@ abstract class PuzzleTest<Input>(
 
     @Test
     fun runPart1TestCases() {
-        part1TestCases.forEach { (inputFile, expected) ->
-            val input = implementation.mapInput(inputFile.load())
-
-            val actual = runCatching {
-                implementation.part1(input)
-            }.getOrElse {
-                fail("An exception was thrown for input $inputFile: ${it.stackTraceToString()}")
-            }
-
-            assertEquals(expected, actual, "Wrong output for $inputFile:")
+        part1TestCases.forEach { (inputFileName, expected) ->
+            runTestCase(inputFileName, expected, implementation::part1)
         }
     }
 
     @Test
     fun runPart2TestCases() {
-        part2TestCases.forEach { (inputFile, expected) ->
-            val input = implementation.mapInput(inputFile.load())
-
-            val actual = runCatching {
-                implementation.part2(input)
-            }.getOrElse {
-                fail("An exception was thrown for input $inputFile: ${it.stackTraceToString()}")
-            }
-
-            assertEquals(expected, actual, "Wrong output for $inputFile:")
+        part2TestCases.forEach { (inputFileName, expected) ->
+            runTestCase(inputFileName, expected, implementation::part2)
         }
     }
 
-    private fun String.load(): List<String> {
+    private fun runTestCase(
+        inputFileName: String,
+        expected: Any,
+        action: (input: Input) -> Any,
+    ) {
+        val input = implementation.mapInput(inputFileName.loadFileContent())
+
+        val timedValue = runCatching {
+            measureTimedValue { action(input) }
+        }.getOrElse {
+            fail("An exception was thrown for input $inputFileName: ${it.stackTraceToString()}")
+        }
+
+        println("$inputFileName took ${timedValue.duration}")
+        assertEquals(expected, timedValue.value, "Wrong output for $inputFileName:")
+    }
+
+    private fun String.loadFileContent(): List<String> {
         val res = PuzzleTest::class.java.classLoader.getResource(this)
         check(res != null) { "Couldn't load resource: $this" }
         return File(res.toURI()).readLines()
